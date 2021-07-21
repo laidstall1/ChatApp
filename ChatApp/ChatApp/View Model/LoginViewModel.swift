9d4996a1babcfcd85e31c52ed95e10f3f6bbf17d
Oluwatoni <laidstall@gmail.com>
2021-07-21 10:48:56 +0100

    complete signup screen, add upload photo functionality, disable login and signup buttons until forms are filled.

--- ChatApp/ChatApp/View Model/LoginViewModel.swift
+++ ChatApp/ChatApp/View Model/LoginViewModel.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
+protocol AuthenticationProtocol {
+    var formIsValid: Bool { get }
+}
+
 struct LoginViewModel {
     var email: String?
     var password: String?
+    
+    var formIsValid: Bool {
+        return email?.isEmpty == false && password?.isEmpty == false 
+    }
 }
