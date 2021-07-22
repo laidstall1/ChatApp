f21bc4ac6968e89b353bea1b82e274d56d4dd1e8
Oluwatoni <laidstall@gmail.com>
2021-07-22 02:28:20 +0100

    refactor: create an AuthService to handle firebase  create user, login user, logout user functionalities

---
+++ ChatApp/ChatApp/API/AuthService.swift
@@ -0,0 +1,20 @@
+//
+//  AuthService.swift
+//  ChatApp
+//
+//  Created by mac on 22/07/2021.
+//
+
+import Foundation
+
+struct AuthService {
+    static let shared = AuthService()
+    
+    func logUserIn() {
+        
+    }
+    
+    func createUser() {
+        
+    }
+}
